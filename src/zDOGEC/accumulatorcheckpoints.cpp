8f63b18426984512b48025d4dc66e238d17d5bea
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-17 21:46:43 -0500

    Update accumulatorcheckpoints.cpp

--- src/zDOGEC/accumulatorcheckpoints.cpp
+++ src/zDOGEC/accumulatorcheckpoints.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "accumulatorcheckpoints.h"
-#include "accumulatorcheckpoints.json.h"
+#include "zDOGEC/accumulatorcheckpoints.json.h"
 
 namespace AccumulatorCheckpoints
 {
