bb93bf610767a24f2041756726a333d911b29007
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-20 10:14:07 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -272,7 +272,7 @@ vFixedSeeds.clear();
         nMintRequiredConfirmations = 20; //the maximum amount of confirmations until accumulated in 19
         nRequiredAccumulation = 1;
         nDefaultSecurityLevel = 100; //full security level for accumulators
-        nZerocoinHeaderVersion = 0; //Block headers must be this version once zerocoin is active
+        nZerocoinHeaderVersion = 4; //Block headers must be this version once zerocoin is active
         nZerocoinRequiredStakeDepth = 200; //The required confirmations for a zdogec to be stakable
 
         nBudget_Fee_Confirmations = 6; // Number of confirmations for the finalization fee
