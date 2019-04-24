5588a698a9437c913dd36cee896fafa6e5660154
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-24 09:01:21 -0500

    Update rpcconsole.cpp

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -639,7 +639,7 @@ void RPCConsole::clear()
     QString clsKey = "Ctrl-L";
 #endif
 
-    message(CMD_REPLY, (tr("Welcome to the dogecash RPC console.") + "<br>" +
+    message(CMD_REPLY, (tr("Welcome to the DogeCash RPC console.") + "<br>" +
                         tr("Use up and down arrows to navigate history, and %1 to clear screen.").arg("<b>"+clsKey+"</b>") + "<br>" +
                         tr("Type <b>help</b> for an overview of available commands.") +
                         "<br><span class=\"secwarning\"><br>" +
