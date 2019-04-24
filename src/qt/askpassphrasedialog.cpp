cb49619f9a652cb4a605c00fd3f77127f1dc9bae
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-24 08:51:35 -0500

    Update askpassphrasedialog.cpp

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -131,7 +131,7 @@ void AskPassphraseDialog::accept()
                 if (model->setWalletEncrypted(true, newpass1)) {
                     QMessageBox::warning(this, tr("Wallet encrypted"),
                         "<qt>" +
-                            tr("dogecash will close now to finish the encryption process. "
+                            tr("DogeCash will close now to finish the encryption process. "
                                "Remember that encrypting your wallet cannot fully protect "
                                "your DOGECs from being stolen by malware infecting your computer.") +
                             "<br><br><b>" +
