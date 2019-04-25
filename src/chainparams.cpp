411525b8951d20bc3a5662d52139a74b413a6736
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-25 13:11:33 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -56,7 +56,7 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints = 
 	boost::assign::map_list_of
-	(0, uint256("0x01"));
+	(0, uint256("0000028e170fb6296d00c266be9c55560c7b82b92c2daaa86cc32d43e4f5e8ff"));
 /*    (259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"))
     (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))
     (616764, uint256("29dd0bd1c59484f290896687b4ffb6a49afa5c498caf61967c69a541f8191557")) //first block to use modifierV2
@@ -194,9 +194,9 @@ public:
         genesis.nVersion = 1;
         genesis.nTime = 1556215485; 
         genesis.nBits = 0x1e0ffff0;
-	genesis.nNonce = 5002474; 
+	genesis.nNonce = 6067418; 
 	
-	hashGenesisBlock = uint256("0x01");
+	/*hashGenesisBlock = uint256("0x01");
         if(genesis.GetHash() != uint256("0x01"))
         {
         printf("Searching for genesis block...\n");
@@ -221,18 +221,18 @@ public:
         printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
         printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
 	system("pause");
-        }
+        }*/
 
 	
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x01")); 
+        assert(hashGenesisBlock == uint256("0000028e170fb6296d00c266be9c55560c7b82b92c2daaa86cc32d43e4f5e8ff")); 
         assert(genesis.hashMerkleRoot == uint256("9d0728cfcb789dd4f8ea183db9ad208c3e6425b6abdf368045960f63693a160c")); 
 
 	vFixedSeeds.clear();
         vSeeds.clear();
-        //vSeeds.push_back(CDNSSeedData("96.30.197.146", "96.30.197.146"));     // Primary DNS Seeder
-        //vSeeds.push_back(CDNSSeedData("80.240.30.55", "80.240.30.55"));    // Secondary DNS Seeder
-        //vSeeds.push_back(CDNSSeedData("45.63.91.15", "45.63.91.15"));         // Single node address
+        vSeeds.push_back(CDNSSeedData("96.30.197.146", "96.30.197.146"));     // Primary DNS Seeder
+        vSeeds.push_back(CDNSSeedData("80.240.30.55", "80.240.30.55"));    // Secondary DNS Seeder
+        vSeeds.push_back(CDNSSeedData("45.63.91.15", "45.63.91.15"));         // Single node address
        // vSeeds.push_back(CDNSSeedData("", "")); // Single node address
        // vSeeds.push_back(CDNSSeedData("", ""));           // Single node address
 
