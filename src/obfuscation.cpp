99353c6fcbd9230e9d4db59266c9459a82416a4e
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-27 05:37:49 -0500

    Update obfuscation.cpp

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -2113,7 +2113,7 @@ bool CObfuScationSigner::IsVinAssociatedWithPubkey(CTxIn& vin, CPubKey& pubkey)
     uint256 hash;
     if (GetTransaction(vin.prevout.hash, txVin, hash, true)) {
         BOOST_FOREACH (CTxOut out, txVin.vout) {
-            if (out.nValue == 10000 * COIN) {
+            if (out.nValue == 5000 * COIN) {
                 if (out.scriptPubKey == payee2) return true;
             }
         }
