16ebe7f11db55e0d18d391c2f04e7ec457d66866
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-27 05:30:06 -0500

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -79,6 +79,8 @@ public:
     CAmount MaxMoneyOut() const { return nMaxMoneyOut; }
     /** The masternode count that we will allow the see-saw reward payments to be off by */
     int MasternodeCountDrift() const { return nMasternodeCountDrift; }
+    int MasternodeCollateralLimit() const { return nMasternodeCollateralLimit; }
+    int getMinstakeReserve() const { return nMinStakeReserve; 
     /** Make miner stop after a block is found. In RPC, don't return until nGenProcLimit blocks are generated */
     bool MineBlocksOnDemand() const { return fMineBlocksOnDemand; }
     /** In the future use NetworkIDString() for RPC fields */
