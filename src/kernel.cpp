a7a6032d1021329eb17cccc26bd98f8c12a1952b
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-28 07:43:18 -0500

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -32,7 +32,7 @@ unsigned int getIntervalVersion(bool fTestNet)
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
-    boost::assign::map_list_of(0, 0x1e0ffff0);
+    boost::assign::map_list_of(0, 504365040);
 
 // Get time weight
 int64_t GetWeight(int64_t nIntervalBeginning, int64_t nIntervalEnd)
