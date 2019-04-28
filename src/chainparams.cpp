d8c04da07aea48c751f16b13fb187491b0149757
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-28 07:43:34 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -56,7 +56,7 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints = 
 	boost::assign::map_list_of
-	(0, uint256("0x01"));
+	(0, uint256("00000ee264e9fb86af299701b725e6314ac73f1f5e7e3ed41aadf007b28c4cc8"));
 /*    (259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"))
     (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))
     (616764, uint256("29dd0bd1c59484f290896687b4ffb6a49afa5c498caf61967c69a541f8191557")) //first block to use modifierV2
@@ -193,10 +193,10 @@ public:
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
         genesis.nTime = 1556455035; 
-        genesis.nBits = 0x1e0ffff0;
-	genesis.nNonce = 288640; 
+        genesis.nBits = 504365040;
+	genesis.nNonce = 406393; 
 	
-	hashGenesisBlock = uint256("0x01");
+	/*hashGenesisBlock = uint256("0x01");
         if(genesis.GetHash() != uint256("0x01"))
         {
         printf("Searching for genesis block...\n");
@@ -222,12 +222,12 @@ public:
         printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
         printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
 	system("pause");
-        }
+        }*/
 
 	
         hashGenesisBlock = genesis.GetHash();
-        //assert(hashGenesisBlock == uint256("0x01")); 
-        //assert(genesis.hashMerkleRoot == uint256("0x01")); 
+        assert(hashGenesisBlock == uint256("00000ee264e9fb86af299701b725e6314ac73f1f5e7e3ed41aadf007b28c4cc8")); 
+        assert(genesis.hashMerkleRoot == uint256("3f1a92c3d193b8678049669d93dcdfaff0a6f2e29bd90159ef4ec959aad906a8")); 
 
 	vFixedSeeds.clear();
         vSeeds.clear();
