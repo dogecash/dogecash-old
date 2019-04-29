c8a31d93352b1bd5d7f5d65a4acfed31196984e9
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-29 10:11:07 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -56,7 +56,7 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints = 
 	boost::assign::map_list_of
-	(0, uint256("0x01"));
+	(0, uint256("00000b878219f5fbc1e9527dfff8f93701e87e43ae9202e9f91a7a54c2f650fe"));
 /*    (259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"))
     (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))
     (616764, uint256("29dd0bd1c59484f290896687b4ffb6a49afa5c498caf61967c69a541f8191557")) //first block to use modifierV2
@@ -99,7 +99,7 @@ static const Checkpoints::CCheckpointData dataRegtest = {
     0,
     100};
 void MineGenesis(CBlock genesis) {
-if(genesis.GetHash() != uint256("0x01"))
+if(genesis.GetHash() != uint256("00000b878219f5fbc1e9527dfff8f93701e87e43ae9202e9f91a7a54c2f650fe"))
         {
             printf("Looking for genesis block...\n");
             uint256 hashTarget;
@@ -219,13 +219,13 @@ public:
         genesis.nVersion = 1;
         genesis.nTime = 1556549250; 
         genesis.nBits = 0x1e0ffff0;
-	genesis.nNonce = 205320; 
+	genesis.nNonce = 1110795; 
         hashGenesisBlock = genesis.GetHash();
         MineGenesis(genesis);
         //debug code
 	    std::cout << "genesisinfo \n " << genesis.ToString();
-        assert(hashGenesisBlock == uint256("0x001")); 
-        assert(genesis.hashMerkleRoot == uint256("0x01")); 
+        assert(hashGenesisBlock == uint256("00000b878219f5fbc1e9527dfff8f93701e87e43ae9202e9f91a7a54c2f650fe")); 
+        assert(genesis.hashMerkleRoot == uint256("27030c3834508a6a73e8792e779253630b7646a818526cf9958717316e8aefbc")); 
 
 	vFixedSeeds.clear();
         vSeeds.clear();
