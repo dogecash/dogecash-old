62df8d275400d0e38ffcf03a961a778d10ee1420
Akshay <akshaycm@hotmail.com>
2019-07-07 21:06:47 +0530

    Revert "macOS: disable AppNap during sync"
    This reverts commit 0c741bc7757176eb6328bed2cb90fd27cdb686d9.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -276,9 +276,7 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle* networkStyle, QWidget* parent) : QMai
     timerAutoMintIcon->start(10000);
     setAutoMintStatus();
 }
-#ifdef Q_OS_MAC
-    CAppNapInhibitor* m_app_nap_inhibitor = new CAppNapInhibitor;
-#endif
+
 BitcoinGUI::~BitcoinGUI()
 {
     // Unsubscribe from notifications from core
@@ -288,7 +286,6 @@ BitcoinGUI::~BitcoinGUI()
     if (trayIcon) // Hide tray icon, as deleting will let it linger until quit (on Ubuntu)
         trayIcon->hide();
 #ifdef Q_OS_MAC
-    delete m_app_nap_inhibitor;
     delete appMenuBar;
     MacDockIconHandler::cleanup();
 #endif
@@ -927,18 +924,14 @@ void BitcoinGUI::setNumConnections(int count)
 
 void BitcoinGUI::setNumBlocks(int count)
 {
-       // Acquire current block source
-    enum BlockSource blockSource = clientModel->getBlockSource();
-    #ifdef Q_OS_MAC
-    (IsInitialBlockDownload() || blockSource == BLOCK_SOURCE_REINDEX || blockSource == BLOCK_SOURCE_DISK ) ? m_app_nap_inhibitor->disableAppNap() : m_app_nap_inhibitor->enableAppNap();
-    #endif
     if (!clientModel)
         return;
 
     // Prevent orphan statusbar messages (e.g. hover Quit in main menu, wait until chain-sync starts -> garbelled text)
     statusBar()->clearMessage();
 
- 
+    // Acquire current block source
+    enum BlockSource blockSource = clientModel->getBlockSource();
     switch (blockSource) {
     case BLOCK_SOURCE_NETWORK:
         progressBarLabel->setText(tr("Synchronizing with network..."));
