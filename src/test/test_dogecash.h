9b6d2caed063b04db5b0afc1d5657cb5395a2c3a
Akshay <akshaycm@hotmail.com>
2019-08-08 16:48:48 +0300

    add missing test_dogecash.h

---
+++ src/test/test_dogecash.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2015 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DOGEC_TEST_TEST_DOGEC_H
+#define DOGEC_TEST_TEST_DOGEC_H
+
+#include "txdb.h"
+
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+/** Basic testing setup.
+ * This just configures logging and chain parameters.
+ */
+struct BasicTestingSetup {
+    BasicTestingSetup();
+    ~BasicTestingSetup();
+};
+
+/** Testing setup that configures a complete environment.
+ * Included are data directory, coins database, script check threads
+ * and wallet (if enabled) setup.
+ */
+struct TestingSetup: public BasicTestingSetup {
+    CCoinsViewDB *pcoinsdbview;
+    boost::filesystem::path pathTemp;
+    boost::thread_group threadGroup;
+    ECCVerifyHandle globalVerifyHandle;
+
+    TestingSetup();
+    ~TestingSetup();
+};
+
+#endif
