4edcd4d74c4fec35d4174c5093ab8a8ae4d31b94
James Stewart <Liquid369@gmail.com>
2019-08-26 13:33:29 -0500

    Try Again

--- src/chain.h
+++ src/chain.h
@@ -377,7 +377,7 @@ public:
                 unsigned int nEntropyBit = ((GetBlockHash().Get64()) & 1);
                 if (GetBoolArg("-printstakemodifier", false)) {
                     LogPrintf("GetStakeEntropyBit: nHeight=%u hashBlock=%s nEntropyBit=%u\n", nHeight, GetBlockHash().ToString().c_str(), nEntropyBit);
-	    return nEntropyBit;
+	        return nEntropyBit;
 			}
 		}
     }
