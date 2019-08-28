f94bf5787e819fd88ef633e489f91732b610e7fe
James Stewart <Liquid369@gmail.com>
2019-08-28 05:46:58 -0500

    Update var

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -156,7 +156,7 @@ bool CMasternode::UpdateFromNewBroadcast(CMasternodeBroadcast& mnb)
         } else {
             // ... otherwise we need to reactivate our node, do not add it to the list and do not relay
             // but also do not ban the node we get this message from
-            LogPrint("CMasternode::UpdateFromNewBroadcast -- wrong PROTOCOL_VERSION, re-activate your MN: message nProtocolVersion=%d  PROTOCOL_VERSION=%d\n", nProtocolVersion, PROTOCOL_VERSION);
+            LogPrint("CMasternode::UpdateFromNewBroadcast -- wrong PROTOCOL_VERSION, re-activate your MN: message nProtocolVersion=%d  PROTOCOL_VERSION=%d\n", protocolVersion, PROTOCOL_VERSION);
             return false;
         }
     return true;
