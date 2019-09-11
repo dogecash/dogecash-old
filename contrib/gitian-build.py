d751b8ca1a4aa40886ff93fa0a97bbbd849d49ed
akshay <akshaycm@hotmail.com>
2019-09-11 17:58:25 +0300

    fix gitian build

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -23,13 +23,13 @@ def setup():
         programs += ['lxc', 'debootstrap']
     subprocess.check_call(['sudo', 'apt-get', 'install', '-qq'] + programs)
     if not os.path.isdir('gitian.sigs'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash-Project/gitian.sigs.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash/gitian.sigs.git'])
     if not os.path.isdir('dogecash-detached-sigs'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash-Project/dogecash-detached-sigs.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash/dogecash-detached-sigs.git'])
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('dogecash'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash-Project/dogecash.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash/dogecash.git'])
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
