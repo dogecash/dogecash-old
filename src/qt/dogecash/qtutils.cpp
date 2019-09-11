21a684b12a9261357a8cd00af09ca0d07170824f
Akshay <akshaycm@hotmail.com>
2019-09-11 11:31:00 +0300

    to dogec color

--- src/qt/dogecash/qtutils.cpp
+++ src/qt/dogecash/qtutils.cpp
@@ -156,7 +156,7 @@ void updateStyle(QWidget* widget){
 QColor getRowColor(bool isLightTheme, bool isHovered, bool isSelected){
     if(isLightTheme){
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#25ffcd88");
         }else if(isHovered){
             return QColor("#25bababa");
         } else{
@@ -164,7 +164,7 @@ QColor getRowColor(bool isLightTheme, bool isHovered, bool isSelected){
         }
     }else{
         if (isSelected) {
-            return QColor("#25b088ff");
+            return QColor("#25ffcd88");
         }else if(isHovered){
             return QColor("#25bababa");
         } else{
