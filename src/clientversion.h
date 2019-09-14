990e0a70d4a3c2e35cf649e131c0d5d27e46c04f
Akshay <akshaycm@hotmail.com>
2019-09-14 18:35:21 +0300

    dogecx to Dogecash

--- src/clientversion.h
+++ src/clientversion.h
@@ -12,7 +12,7 @@
 
 // Check that required client information is defined
 #if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
-#error Client version information missing: version is not defined by dogecx-config.h or in any other way
+#error Client version information missing: version is not defined by dogecash-config.h or in any other way
 #endif
 
 /**
@@ -26,7 +26,7 @@
 #define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The PIVX Core Developers"
 
 /**
- * dogecxd-res.rc includes this file, but it cannot cope with real c++ code.
+ * dogecashd-res.rc includes this file, but it cannot cope with real c++ code.
  * WINDRES_PREPROC is defined to indicate that its pre-processor is running.
  * Anything other than a define should be guarded below.
  */
