4d03aa78951a6a75872aaa80dd6a0fc279d4ffe1
Akshay CM <akshaycm@hotmail.com>
2019-10-10 22:43:20 +0530

    [Core] Up the stakemodv2 Block height
    To be 2 weeks from now

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -160,7 +160,7 @@ public:
         nBlockDoubleAccumulated = 1050010;
         nEnforceNewSporkKey = 1425158000; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM
-        nBlockNewStakeProtocol = 150000; //!> Enforce new Stake Protocols add test/regtest
+        nBlockNewStakeProtocol = 227769; //!> Enforce new Stake Protocols add test/regtest
 
         // Fake Serial Attack
         nFakeSerialBlockheightEnd = 1686229;
