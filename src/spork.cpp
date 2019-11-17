e347ac879ad622e88e62afd6afdab0ae16d70a64
Akshay <akshaycm@hotmail.com>
2019-11-17 22:05:36 +0300

    Fix chainheight ref

--- src/spork.cpp
+++ src/spork.cpp
@@ -105,7 +105,7 @@ void CSporkManager::ProcessSpork(CNode* pfrom, std::string& strCommand, CDataStr
         // reject old signatures 600 blocks after hard-fork
         if (spork.nMessVersion != MessageVersion::MESS_VER_HASH) {
             if (Params().NewSigsActive(nChainHeight - 600)) {
-                LogPrintf("%s : nMessVersion=%d not accepted anymore at block %d", __func__, spork.nMessVersion, nHeight);
+                LogPrintf("%s : nMessVersion=%d not accepted anymore at block %d", __func__, spork.nMessVersion, nChainHeight);
                 return;
             }
         }
