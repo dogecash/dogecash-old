ecc1c1ec9ba17f83ff48e1e944c80ad965a4abb8
James Stewart <Liquid369@gmail.com>
2020-01-13 08:51:34 -0600

    Merge #1233: [Trivial] Remove spammy log in in StakeV1
    f9857f71f9adc18eff398eb5ec2839a7bd15671e [Trivial] Remove spammy log in in StakeV1 (random-zebra)
    
    Pull request description:
    
      This is not an error, the function should just return false. No need to spam the debug.log.
      This will become a non-issue after time v2 enforcement, but better to fix it anyway.
    
    ACKs for top commit:
      furszy:
         ACK f9857f7
      Fuzzbawls:
        ACK f9857f71f9adc18eff398eb5ec2839a7bd15671e
    
    Tree-SHA512: dc624fd7d81994f7aaac2b1a421000d05ca402d2864985cadbfd44f668971f6f2ed40138aa31615054cef02a71ca9f245eb0b7b19c72aea5ef4a52fcbcaadb36

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -372,8 +372,16 @@ bool Stake(const CBlockIndex* pindexPrev, CStakeInput* stakeInput, unsigned int 
     // but not after the max allowed future blocktime drift (3 minutes for PoS)
     const unsigned int maxTime = std::min(nTimeTx + nHashDrift, Params().MaxFutureBlockTime(GetAdjustedTime(), true));
 
-    while (nTryTime < maxTime)
-    {
+    if (maxTime <= minTime) {
+        // too early to stake
+        return false;
+    }
+
+    while (nTryTime > minTime) {
+        // store a time stamp of when we last hashed on this block
+        mapHashedBlocks.clear();
+        mapHashedBlocks[pindexPrev->nHeight] = GetTime();
+
         //new block came in, move on
         if (chainActive.Height() != prevHeight && Params().NetworkID() == CBaseChainParams::REGTEST)
             break;
