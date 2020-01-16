62d0ff3b448fff3ae8befa7b5f5dc03a366e20a7
James Stewart <Liquid369@gmail.com>
2020-01-16 07:53:47 -0600

    [GUI] Display latest block number in the top bar

--- src/qt/dogecash/topbar.cpp
+++ src/qt/dogecash/topbar.cpp
@@ -414,7 +414,7 @@ void TopBar::setNumBlocks(int count) {
         if (masternodeSync.IsSynced()) {
             // Node synced
             // TODO: Set synced icon to pushButtonSync here..
-            ui->pushButtonSync->setButtonText(tr("Synchronized"));
+            ui->pushButtonSync->setButtonText(tr("Synchronized - Block: %1").arg(QString::number(count)));
             progressBar->setRange(0,100);
             progressBar->setValue(100);
             return;
@@ -427,7 +427,7 @@ void TopBar::setNumBlocks(int count) {
             int progress = nAttempt + (masternodeSync.RequestedMasternodeAssets - 1) * MASTERNODE_SYNC_THRESHOLD;
             if(progress >= 0){
                 // todo: MN progress..
-                text = std::string("Synchronizing additional data..");//: %p%", progress);
+                text = strprintf("Synchronizing masternodes data... - Block: %d", count);
                 //progressBar->setMaximum(4 * MASTERNODE_SYNC_THRESHOLD);
                 //progressBar->setValue(progress);
                 needState = false;
