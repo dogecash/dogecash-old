fc3444c099c215f23e6ad2a1d34f1fb6c2acd673
furszy <matiasfurszyfer@protonmail.com>
2020-04-16 16:46:17 -0300

    Backport zkSNARK compression.

---
+++ src/sapling/proof.cpp
@@ -0,0 +1,13 @@
+#include "sapling/proof.hpp"
+
+namespace libzcash {
+
+ProofVerifier ProofVerifier::Strict() {
+    return ProofVerifier(true);
+}
+
+ProofVerifier ProofVerifier::Disabled() {
+    return ProofVerifier(false);
+}
+
+}
