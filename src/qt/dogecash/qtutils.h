ca61873fc55817b66f3968caa58a21bcb8388152
Liquid369 <Liquid369@gmail.com>
2020-04-29 04:36:10 -0500

    [GUI] Customizable sorting for address books

--- src/qt/dogecash/qtutils.h
+++ src/qt/dogecash/qtutils.h
@@ -43,7 +43,8 @@ void setupSettings(QSettings *settings);
 bool isLightTheme();
 void setTheme(bool isLight);
 
-void initComboBox(QComboBox* combo, QLineEdit* lineEdit = nullptr);
+void initComboBox(QComboBox* combo, QLineEdit* lineEdit = nullptr, QString cssClass = "btn-combo");
+void fillAddressSortControls(SortEdit* seType, SortEdit* seOrder, QComboBox* boxType, QComboBox* boxOrder);
 
 // filters
 void setFilterAddressBook(QComboBox* filter, SortEdit* lineEdit);
