4706d054f7cf9a4d4a7e4817dc3eb3e4a7ba00f2
Liquid369 <Liquid369@gmail.com>
2020-12-03 14:27:45 -0600

    Bump

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70921;
+static const int PROTOCOL_VERSION = 70922;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,8 +21,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70920;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70921;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70921;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70922;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70915;
